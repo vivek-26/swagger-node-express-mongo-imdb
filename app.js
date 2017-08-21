bb331c405dc9a8b374b770a1bfb5bf9bf4d0fe55
Vivek Kumar <vivek.kumar26@live.com>
2017-08-21 23:53:09 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var SwaggerExpress = require('swagger-express-mw');
+var app = require('express')();
+var mongodb = require('./db/mongo');
+module.exports = app; // for testing
+
+var config = {
+   appRoot: __dirname // required config
+};
+
+SwaggerExpress.create(config, function (err, swaggerExpress) {
+   if (err) {
+      throw err;
+   }
+
+   // install middleware
+   swaggerExpress.register(app);
+
+   var port = process.env.PORT || 10010;
+
+   // Connect to MongoDB first and then listen for requests
+   createConnection(port);
+
+   // if (swaggerExpress.runner.swagger.paths['/hello']) {
+   //   console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+   // }
+});
+
+/**
+ * @function createConnection
+ * @description Creates a new MongoDB connection
+ * @param {Integer} port 
+ */
+async function createConnection(port) {
+   let res = await mongodb.connect();
+   if (res !== null) {
+      console.error('Failed to connect to MongoDB instance');
+      process.exit(1);
+   } else {
+      app.listen(port);
+      console.info('server started on port: ', port);
+   }
+};
---
+++ db/mongo.js
@@ -0,0 +1,46 @@
+/**
+ * @author Vivek Kumar
+ * Module Dependencies
+ */
+var mongoClient = require('mongodb').MongoClient;
+var dbConfig = require('./config');
+
+// Global variable for MongoDB Instance
+var _db;
+
+// Connection URL
+var connectionURL = `mongodb://${dbConfig.username}:${dbConfig.password}` +
+   `@${dbConfig.host}:${dbConfig.port}/${dbConfig.database}`;
+// console.info('connectionURL', connectionURL);
+
+/**
+ * @function connect
+ * @description Connect to MongoDB Database and set the global db object
+ */
+async function connect() {
+   try {
+      _db = await mongoClient.connect(connectionURL);
+      if (_db) {
+         return null;
+      } else {
+         return 'Connection Failed!';
+      }
+   } catch (err) {
+      console.error(err);
+      return err;
+   }
+};
+
+/**
+ * @function getDBInstance
+ * @description Return the global db object
+ */
+function getDBInstance() {
+   return _db;
+};
+
+// Export functions
+module.exports = {
+   connect,
+   getDBInstance
+};
