a9aa7d80f8e122c3f2854d79496c92f5d1860620
Vivek Kumar <vivek.kumar26@live.com>
2017-09-04 19:06:02 +0530

    js code & model update- get movie by genre

--- api/controllers/getMovieListByGenre.js
+++ api/controllers/getMovieListByGenre.js
@@ -15,13 +15,15 @@ var bunyan = require('./../../logger/bunyan');
  */
 function fetchMovieListByGenre(req, res) {
    let genre = req.swagger.params.genre.value;
+   let pageNumber = req.swagger.params.page.value || 1;
+   let numOfRecords = req.swagger.params.count.value || 10;
    let logger = bunyan.getLogger(),
       id_route = {
          reqId: req.uuidTimestamp,
          route: req.url
       };
-   logger.info(id_route, `Genre: ${genre}`);
-   fetchMovieList(id_route, genre, function (err, movieList) {
+   logger.info(id_route, `Genre: ${genre}, Page Number: ${pageNumber}, Count: ${numOfRecords}`);
+   fetchMovieList(id_route, genre, pageNumber, numOfRecords, function (err, movieList) {
       if (err) {
          return res.status(err.statusCode).json({
             error: err.message
@@ -31,12 +33,60 @@ function fetchMovieListByGenre(req, res) {
    });
 };
 
-async function fetchMovieList(id_route, genre, callback) {
-   // Return dummy data for now
-   return callback(null, {
-      movieList: ['Dummy', 'Data', 'Here']
-   });
-};
+async function fetchMovieList(id_route, genre, pageNumber, numOfRecords, callback) {
+   let logger = bunyan.getLogger();
+   try {
+      let db = mongo.getDBInstance();
+      let limit = pageNumber * numOfRecords,
+         skip = (pageNumber - 1) * numOfRecords;
+      let movieList = await db.collection(MOVIES).aggregate([{
+         $match: {
+            $and: [{
+               'Genres': genre
+            }, {
+               'SeriesType': 'F'
+            }, {
+               'ReleaseYear': {
+                  $gt: new Date().getFullYear() - 2,
+                  $lt: new Date().getFullYear() + 1
+               }
+            }]
+         }
+      }, {
+         $sort: {
+            'ReleaseYear': -1,
+            'Rating.RatingVotes': -1
+         }
+      }, {
+         $project: {
+            '_id': 0,
+            'MovieID': 1,
+            'Rating.Rating': 1
+         }
+      }, {
+         $limit: limit
+      }, {
+         $skip: skip
+      }]).toArray();
+      if ((Object.prototype.toString.call(movieList) === '[object Array]') &&
+         movieList.length > 0) {
+         logger.info(id_route, `Found ${movieList.length} records for Genre: ${genre}!`);
+         return callback(null, movieList);
+      } else {
+         logger.error(id_route, `Could not fetch a list of movies for Genre: ${genre}`);
+         return callback({
+            statusCode: 500,
+            message: `Could not get movie list for genre ${genre}`
+         });
+      }
+   } catch (err) {
+      logger.error(id_route, err.message);
+      return callback({
+         statusCode: 500,
+         message: err.message
+      });
+   }
+}
 
 // Export route handler
 module.exports = {
